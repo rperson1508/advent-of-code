cda2f2231ce2ab4103c7ac8fecf0282aaf6d81c9
invalid-user <77624311+rperson1508@users.noreply.github.com>
2022-12-01 21:22:09 +0800

    2022/d1 complete!

---
+++ 2022/solutions/d1.py
@@ -0,0 +1,10 @@
+inp: str = open('2022/inputs/d1.txt').read()
+
+elfs = [i.strip() for i in inp.split('\n\n')]
+cals = sorted([sum(int(i or 0) for i in e.split('\n')) for e in elfs])
+
+# Part 1
+print(cals[-1])
+
+# Part 2
+print(cals[-1] + cals[-2] + cals[-3])
