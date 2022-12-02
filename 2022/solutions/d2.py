ea06894920822776eb9f470fbc0ecea1f8909e7f
invalid-user <77624311+rperson1508@users.noreply.github.com>
2022-12-02 20:20:06 +0800

    2022/d2 done

---
+++ 2022/solutions/d2.py
@@ -0,0 +1,39 @@
+from typing import Optional, Literal, Tuple
+
+inp: str = open('2022/inputs/d2.txt').read()
+rounds = [i.split() for i in inp.split('\n')]
+
+moves = {'X': 1, 'Y': 2, 'Z': 3,}
+outcome = {True: 6, None: 3, False: 0,}
+
+# Part 1
+
+def get_outcome(opp: str, me: str) -> Optional[bool]:
+    if 'ABC'.index(opp) == 'XYZ'.index(me): 
+        status = None
+    elif ('ABC'.index(opp) + 1) == 'XYZ'.index(me) or ((opp, me) == ('C', 'X')):
+        status = True
+    elif ('ABC'.index(opp) - 1) == 'XYZ'.index(me) or ((opp, me) == ('A', 'Z')):
+        status = False
+    return status
+
+print(sum((moves.get(me) + outcome.get(get_outcome(opp, me))) for opp, me in rounds))
+
+# Part 2
+
+wins = {'A': 'Y', 'B': 'Z', 'C': 'X',}
+lose = {'A': 'Z', 'B': 'X', 'C': 'Y',}
+
+def get_move(opp: str, end: str) -> Tuple[Literal['X', 'Y', 'Z'], Optional[bool]]:
+    result = {'X': False, 'Y': None, 'Z': True}
+    end: Optional[bool] = result.get(end)
+
+    if end is None:  # draw
+        move = list(moves.keys())['ABC'.index(opp)]
+    elif end is True:  # win
+        move = wins[opp]
+    elif end is False:  # lose
+        move = lose[opp]
+    return (move, end)
+
+print(sum((moves.get(get_move(opp, end)[0]) + outcome.get(get_move(opp, end)[1])) for opp, end in rounds))
